7994459c33dafa15123aae3ab551d341e3152f94
Muhammad Sami-Ud-Din <muhammadsmmdn2007@gmail.com>
2026-07-24 20:49:25 +0500

    list rendering

--- my-perfect-app/src/App.jsx
+++ my-perfect-app/src/App.jsx
@@ -1,71 +1,71 @@
-import profilePic from './WhatsApp Image 2026-04-17 at 21.20.43.jpeg';
-// function MyButton() {
-//   return (
-//     <button className="sami">
-//       Click me
-//       </button>
-//   );
-// }
+// import profilePic from './WhatsApp Image 2026-04-17 at 21.20.43.jpeg';
+// // function MyButton() {
+// //   return (
+// //     <button className="sami">
+// //       Click me
+// //       </button>
+// //   );
+// // }
+
+// // export default function App(){
+// //   return(
+// //     <div>
+// //       <h1>Ye mera area hai aur mere area mein koi kaddam rkhne ka nahi</h1>
+// //       <MyButton />
+// //     </div>
+// //   );
+// // }
+
+
+
+// //escaping to javascript from jsx
+// const users = [{
+//  Name: "Muhammad Sami ud din",
+//   age : 19,
+//   image: profilePic
+// },{
+//   Name: "Muhammad Zorain Haider",
+//   age: 25,
+//   image: profilePic
+// }];
+
+
+
+
+// // function MyButton() {
+// //   return (
+// //     <button className="sami">
+// //       Click me
+// //       </button>
+// //   );
+// // }
 
 // export default function App(){
-//   return(
-//     <div>
-//       <h1>Ye mera area hai aur mere area mein koi kaddam rkhne ka nahi</h1>
-//       <MyButton />
-//     </div>
-//   );
+// return(
+//   <div>
+//  { 
+//   users.map((user) => {
+//   if(user.age < 20){
+//     return(
+//       <div>
+//         <h1>{user.Name}</h1>
+//         <h1>{user.age}🫣junior</h1>
+//         <img src={user.image} alt="Profile"  className="avatar"/>
+//       </div>
+//     );
+//   }else{
+//     return(
+//       <div>
+//         <h1 >{user.Name}</h1>
+//         <h1>{user.age}😂senior</h1>
+//         <img src={user.image} alt="Profile"  className="avatar"/>
+//       </div>
+//     );
+//   }})
 // }
-
-
-
-//escaping to javascript from jsx
-const users = [{
- Name: "Muhammad Sami ud din",
-  age : 19,
-  image: profilePic
-},{
-  Name: "Muhammad Zorain Haider",
-  age: 25,
-  image: profilePic
-}];
-
-
-
-
-// function MyButton() {
-//   return (
-//     <button className="sami">
-//       Click me
-//       </button>
-//   );
+// </div>
+// )
 // }
-
-export default function App(){
-return(
-  <div>
- { 
-  users.map((user) => {
-  if(user.age < 20){
-    return(
-      <div>
-        <h1>{user.Name}</h1>
-        <h1>{user.age}🫣junior</h1>
-        <img src={user.image} alt="Profile"  className="avatar"/>
-      </div>
-    );
-  }else{
-    return(
-      <div>
-        <h1 >{user.Name}</h1>
-        <h1>{user.age}😂senior</h1>
-        <img src={user.image} alt="Profile"  className="avatar"/>
-      </div>
-    );
-  }})
-}
-</div>
-)
-}
 
 
 
@@ -82,3 +82,44 @@ return(
     
 //     }
 
+
+
+
+
+
+
+//Rendering list items in react using map function
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+
+
+
+export default function productList() {
+  
+  
+   const ProductItems = products.map(product => 
+      <li key= {product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'pink'
+      }}>
+        {product.title}
+        </li>
+    );
+  
+  
+  return (
+    <>
+    <ul>{ProductItems}</ul>
+    
+    
+    </>
+    
+  );
+  
+  
+   
+}
