4a8e7fb571874aaa149d2462da70142c374e5fc1
Muhammad Sami-Ud-Din <muhammadsmmdn2007@gmail.com>
2026-07-28 01:40:55 +0500

    react router

--- my-perfect-app/src/main.jsx
+++ my-perfect-app/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
+import MyAppNav from "./MyAppNav";
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <StrictMode>
     <App />
+    <MyAppNav />
   </StrictMode>
   </BrowserRouter>
 )
